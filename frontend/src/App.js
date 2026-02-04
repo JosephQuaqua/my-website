6a1c00a0b3c1314a2ba827b8758f974f00b6e5d4
JosephQuaqua <josephquaqua2004@gmail.com>
2026-02-04 18:00:55 +0530

    deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+// Change HashRouter back to BrowserRouter
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
@@ -7,59 +8,32 @@ import ForgotPassword from "./pages/ForgetPassword";
 import Dashboard from "./pages/Dashboard";
 import Savings from "./pages/Savings";
 import Loan from "./pages/Loan";
+import PayLoan from "./pages/PayLoan";
+import ApplyLoan from "./pages/ApplyLoan";
+import SavingPayment from "./pages/SavingPayment";
+import LoanApplication from "./pages/LoanApplication";
 import Investment from "./pages/Investment";
 import Exchange from "./pages/Exchange";
-import Monitors from "./pages/Monitors";
-
-import ProtectedRoute from "./components/ProtectedRoute";
+import Monitors from "./pages/Monitors"; // Your new page
 
 function App() {
   return (
+    /* Restore the basename so your links work within the subfolder */
     <BrowserRouter basename="/my-website">
       <Routes>
-
-        {/* Public Routes */}
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-
-        {/* Protected Routes */}
-        <Route path="/dashboard" element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/savings" element={
-          <ProtectedRoute>
-            <Savings />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/loan" element={
-          <ProtectedRoute>
-            <Loan />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/investment" element={
-          <ProtectedRoute>
-            <Investment />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/exchange" element={
-          <ProtectedRoute>
-            <Exchange />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/monitors" element={
-          <ProtectedRoute>
-            <Monitors />
-          </ProtectedRoute>
-        } />
-
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/savings" element={<Savings />} />
+        <Route path="/loan" element={<Loan />} />
+        <Route path="/pay-loan" element={<PayLoan />} />
+        <Route path="/apply-loan" element={<ApplyLoan />} />
+        <Route path="/saving-payment" element={<SavingPayment />} />
+        <Route path="/loan-application" element={<LoanApplication />} />
+        <Route path="/investment" element={<Investment />} />
+        <Route path="/exchange" element={<Exchange />} />
+        <Route path="/monitors" element={<Monitors />} />
       </Routes>
     </BrowserRouter>
   );
